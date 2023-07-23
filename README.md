05e0cd956ab0c53afc28e1ac82520883dce5b5fa
Abdallh Izzeldin <abdodx126@gmail.com>
2023-07-23 23:15:08 +0200

    initializng Quiz-App

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Random-quiz
+# Quiz App
+
+A responsive quiz app that lets you choose a topic and gives you random questions from the Trivia API. Test your knowledge in various subjects like General Knowledge, Science, History, and more.
+
+## Features
+
+- Choose a topic from the dropdown menu.
+- Start the quiz to receive random questions related to the selected topic.
+- Answer the questions and get immediate feedback on correctness.
+- If your answer is wrong, the app provides the correct answer for learning purposes.
+- After completing the quiz, see your overall score and the number of correct answers.
+
+## How to Use
+
+1. Clone the repository: `git clone https://github.com/WDezzo/Random-quiz.git`
+2. Open `index.html` in your web browser.
+3. Choose a topic from the dropdown menu and click "Start Quiz."
+4. Answer the questions one by one and get immediate feedback.
+5. After completing the quiz, see your overall score and the number of correct answers.
+6. To restart the quiz, click the "Restart Quiz" button.
+
+## API Source
+
+This app uses the Trivia API (https://the-trivia-api.com/v2/questions) to fetch random questions based on the selected topic.
+
+## Credits
+
+This project was created by WDezzo. Feel free to use and modify it as you like.
+Thank `https://the-trivia-api.com/` for provide the quiz
---
+++ script.js
@@ -0,0 +1,89 @@
+// API URL to fetch random questions
+const apiUrl = "https://the-trivia-api.com/v2/questions";
+
+// DOM elements
+const quizContainer = document.getElementById("quiz-container");
+const questionElement = document.getElementById("question");
+const answersElement = document.getElementById("answers");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestion = {};
+
+// Function to fetch a random question from the API
+async function getQuestion(apiUrl) {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    currentQuestion = data[0];
+    showQuestion(currentQuestion);
+  } catch (error) {
+    console.error("Error fetching question:", error);
+  }
+}
+
+// Function to display the current question and answer options
+function showQuestion(data) {
+  questionElement.textContent = data.question;
+  answersElement.innerHTML = "";
+  let answerList=[];
+  for(let i=0; i<4; i++){
+    i===0?answerList.push(data.correctAnswer):answerList.push(data.incorrectAnswers);
+  }
+  answerList = answerList.sort((a, b) => 0.5 - Math.random());
+  answerList.forEach((answer) => {
+    const button = document.createElement("button");
+    button.textContent = answer;
+    button.classList.add("btn", "btn-light");
+    button.addEventListener("click", checkAnswer);
+    answersElement.appendChild(button);
+  });
+}
+
+// Function to check the selected answer and provide feedback
+function checkAnswer(event) {
+  const selectedAnswer = event.target.textContent;
+  const correctAnswer = currentQuestion.correctAnswer;
+
+  if (selectedAnswer === correctAnswer) {
+    event.target.classList.add("correct");
+    showFeedback("Correct!", true);
+  } else {
+    event.target.classList.add("incorrect");
+    showFeedback(`Incorrect! The correct answer is: ${correctAnswer}`, false);
+  }
+
+  // Disable all answer buttons after selection
+  document.querySelectorAll("#answers button").forEach((button) => {
+    button.disabled = true;
+  });
+
+  nextButton.style.display = "block";
+  nextButton.addEventListener("click", nextQuestion);
+}
+
+// Function to show feedback after user selects an answer
+function showFeedback(feedbackText, isCorrect) {
+  const feedbackElement = document.createElement("div");
+  feedbackElement.textContent = feedbackText;
+  feedbackElement.classList.add("mt-3", "font-weight-bold", isCorrect ? "text-success" : "text-danger");
+  quizContainer.appendChild(feedbackElement);
+}
+
+// Function to move to the next question
+function nextQuestion() {
+  // Enable all answer buttons and remove any highlighting
+  document.querySelectorAll("#answers button").forEach((button) => {
+    button.disabled = false;
+    button.classList.remove("correct", "incorrect");
+  });
+
+  // Hide the feedback from the previous question
+  nextButton.style.display = "none";
+  quizContainer.removeChild(quizContainer.lastChild);
+
+  // Fetch the next question
+  getQuestion(apiUrl);
+}
+
+// Fetch the first question to start the quiz
+getQuestion(apiUrl);
